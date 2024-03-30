8d9bc8bc27ee8f923f36984a9eba11e3ab4b4a4e
Ang Chen <chenang@outlook.com>
2024-03-30 15:38:29 +0800

    Update test.cu in 1.02/

--- learn-lesson-1.02/test.cu
+++ learn-lesson-1.02/test.cu
@@ -2,13 +2,13 @@
 
 __global__ void hello_from_gpu()
 {
-    printf("Hello World from the the GPU\n");
+    printf("'Hello World' from the the GPU\n");
 }
 
 
 int main(void)
 {
-    hello_from_gpu<<<4, 4>>>();
+    hello_from_gpu<<<4, 2>>>();
     cudaDeviceSynchronize();
 
     return 0;
