5242676e05997fbc9f4d3b4af0204307832439c2
Ang Chen <chenang@outlook.com>
2024-04-01 13:09:08 +0800

    Update ex2.cu

--- learn-lesson-2.03/ex2.cu
+++ learn-lesson-2.03/ex2.cu
@@ -6,7 +6,7 @@ __global__ void hello_from_gpu()
     const int tid = threadIdx.x;
 
     const int id = threadIdx.x + blockIdx.x * blockDim.x; 
-    printf("Hello World from block %d and thread %d, global id %d\n", bid, tid, id);
+    printf("Hello World from block %d and thread %d, global id %d of GPU\n", bid, tid, id);
 }
 
 
